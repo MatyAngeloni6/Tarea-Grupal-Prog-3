318aa795ef9c90f4bf10f00d7f28b48be9ef5145
Ana Bellavillaverde <b.villaverde@itecriocuarto.org.ar>
2026-04-13 15:02:28 -0300

    agregue componente footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="w-full border-t bg-white py-6 mt-auto">
+      <p className="text-center text-sm text-slate-500">
+        Derechos reservados Copyright F1 - 2026
+      </p>
+    </footer>
+  );
+}
